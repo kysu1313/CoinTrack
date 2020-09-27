8f443aec224a81e5751bec3d84f289c7633e1d90
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2020-09-26 23:22:35 -0400

    Revert "dashboard, work in progress"

--- tabControllers/assistantControllers/Tab1AssistantController.java
+++ tabControllers/assistantControllers/Tab1AssistantController.java
@@ -1,4 +1,3 @@
-
 package tabControllers.assistantControllers;
 
 import coinClasses.ConnectToDatabase;
@@ -115,84 +114,6 @@ public class Tab1AssistantController {
             });
             return row;
         });
-    }
-    
-    public void coinTableDash(TableView tableViewT1, LinkedList<SingleCoin> coinList) {
-        
-        // Create columns
-        TableColumn col1 = new TableColumn("Name");
-        TableColumn col2 = new TableColumn("Symbol");
-        TableColumn col3 = new TableColumn("Price (USD)");
-        TableColumn col5 = new TableColumn("Change");
-        // Link columns to properties in SingleCoin class
-        col1.setCellValueFactory(new PropertyValueFactory<>("name"));
-        col2.setCellValueFactory(new PropertyValueFactory<>("symbol"));
-        col3.setCellValueFactory(new PropertyValueFactory<>("price"));
-//        col4.setCellValueFactory(new PropertyValueFactory<>("rank"));
-        col5.setCellValueFactory(new PropertyValueFactory<>("stringChange"));
-//        col6.setCellValueFactory(new PropertyValueFactory<>("volume"));
-        // Add columns to tableView
-        tableViewT1.getColumns().addAll(col2, col3, col5);
-        ObservableList<SingleCoin> obvList = FXCollections.observableArrayList(coinList);
-        // Change text color of "change" column if positive or negative change.
-        col5.setCellFactory(new Callback<TableColumn, TableCell>() {
-            public TableCell call(TableColumn param) {
-                return new TableCell<SingleCoin, String>() {
-
-                    @Override
-                    public void updateItem(String item, boolean empty) {
-                        super.updateItem(item, empty);
-                        // Change color based on data
-                        if (!isEmpty()) {
-                            this.setStyle("-fx-text-fill: #09de57;-fx-font-weight: bold;");
-                            if (item.contains("-")) {
-                                this.setStyle("-fx-text-fill: #ff0000;-fx-font-weight: bold;");
-                            }
-                            setText(item);
-                        }
-                    }
-                };
-            }
-        });
-//        col3.setCellFactory(new Callback<TableColumn, TableCell>() {
-//            public TableCell call(TableColumn param) {
-//                return new TableCell<SingleCoin, String>() {
-//
-//                    @Override
-//                    public void updateItem(String item, boolean empty) {
-//                        super.updateItem(item, empty);
-//                        // Change color based on data
-//                        if (!isEmpty()) {
-//                            // This is a SUPER "hacky" way to change prices LOL
-//                            String newPrice = String.format("%.5f", Float.parseFloat(item)*Float.parseFloat(""+currencyRate));
-//                            setText(newPrice);
-//                        }
-//                    }
-//                };
-//            }
-//        });
-        tableViewT1.setItems(obvList);
-        tableViewT1.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
-        // Allows user to double click a table row and display info in textArea
-//        tableViewT1.setRowFactory(tv -> {
-//            TableRow<SingleCoin> row = new TableRow<>();
-//            row.setOnMouseClicked(new EventHandler<MouseEvent>() {
-//                @Override
-//                public void handle(MouseEvent event) {
-//                    if (event.getClickCount() == 2 && (!row.isEmpty())) {
-//                        SingleCoin rowData = row.getItem();
-//                        System.out.println(rowData);
-//                        String imgPath = rowData.getIconUrl();
-//
-//                        // Attempting to resize the coin logo image.
-//                        webViewT1.setPrefHeight(56);
-//                        webViewT1.setPrefWidth(56);
-//                        webViewT1.getEngine().load(imgPath);
-//                    }
-//                }
-//            });
-//            return row;
-//        });
     }
 
 }
