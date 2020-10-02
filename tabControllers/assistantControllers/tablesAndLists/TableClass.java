0c99e58e2d2e84dd4080eeab4b7816a9469f34cd
kysu1313 <ksups111@gmail.com>
2020-10-02 19:29:34 -0400

    Table Class is mostly* complete.

--- tabControllers/assistantControllers/tablesAndLists/TableClass.java
+++ tabControllers/assistantControllers/tablesAndLists/TableClass.java
@@ -11,11 +11,14 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableRow;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.web.WebView;
 import javafx.util.Callback;
 
@@ -35,7 +38,7 @@ public class TableClass implements TableInterface{
     // List of columns to be added
     private LinkedList<TableColumn> cols;
     private ObservableList<SingleCoin> obvList;
-    private TableColumn col1;
+    private TableColumn changeCol;
     private TableColumn col2;
     private TableColumn col3;
     private TableColumn col4;
@@ -89,7 +92,7 @@ public class TableClass implements TableInterface{
                 }
             }
         });
-//        colorChangeCol();
+        setCurrency();
         // Add columns to tableView
         this.TABLE_VIEW.getColumns().addAll(this.cols);
         this.obvList = FXCollections.observableArrayList(this.coinList);
@@ -97,26 +100,25 @@ public class TableClass implements TableInterface{
 
     /**
      * Add red / green colored text to indicate price changes.
-     *
-     * NOT WORKING. (trying to parse double to string??)
      */
-    private void colorChangeCol() {
+    @Override
+    public void colorChangeCol(String _colorUp, String _colorDown) {
         this.cols.forEach((item) -> {
             if (item.getText().equalsIgnoreCase("change")) {
                 item.setCellFactory(new Callback<TableColumn, TableCell>() {
                     public TableCell call(TableColumn param) {
-                        return new TableCell<SingleCoin, String>() {
-
+                        return new TableCell<SingleCoin, Number>() {
                             @Override
-                            public void updateItem(String item, boolean empty) {
+                            protected void updateItem(Number item, boolean empty) {
+                                // calling super here is very important - don't skip this!
                                 super.updateItem(item, empty);
                                 // Change color based on data
                                 if (!isEmpty()) {
-                                    this.setStyle("-fx-text-fill: #09de57;-fx-font-weight: bold;");
-                                    if (item.contains("-")) {
-                                        this.setStyle("-fx-text-fill: #ff0000;-fx-font-weight: bold;");
+                                    this.setStyle("-fx-text-fill: " + _colorUp + ";-fx-font-weight: bold;");
+                                    if (item.toString().contains("-")) {
+                                        this.setStyle("-fx-text-fill: " + _colorDown + ";-fx-font-weight: bold;");
                                     }
-                                    setText(item);
+                                    setText(item.toString());
                                 }
                             }
                         };
@@ -126,8 +128,11 @@ public class TableClass implements TableInterface{
         });
     }
 
-    @Override
-    public void setCurrency(long currRate) {
+    /**
+     * Set the currency rate and adjust all prices in the table.
+     * @param currRate
+     */
+    private void setCurrency() {
         // Change text color of "change" column if positive or negative change.
         // Default: col5 and col3
         this.cols.forEach((item) -> {
@@ -142,41 +147,41 @@ public class TableClass implements TableInterface{
                                 // Change color based on data
                                 if (!isEmpty()) {
                                     // This is a SUPER "hacky" way to change prices LOL
-                                    String newPrice = String.format("%.5f", Float.parseFloat(item) * Float.parseFloat("" + currRate));
+                                    String newPrice = String.format("%.5f", Float.parseFloat(item) * Float.parseFloat("" + CURRENCY_RATE));
                                     setText(newPrice);
                                 }
                             }
                         };
+                        
                     }
                 });
             }
         });
     }
-
+    
+    /**
+     * Add a double click that displays the currencies logo.
+     */
     @Override
-    public void setColor(String _colorUp, String _colorDown) {
-        this.cols.forEach((item) -> {
-            if (item.getText().equalsIgnoreCase("change")) {
-                item.setCellFactory(new Callback<TableColumn, TableCell>() {
-                    public TableCell call(TableColumn param) {
-                        return new TableCell<SingleCoin, String>() {
+    public void addDoubleClick() {
+        this.TABLE_VIEW.setRowFactory(tv -> {
+            TableRow<SingleCoin> row = new TableRow<>();
+            row.setOnMouseClicked(new EventHandler<MouseEvent>() {
+                @Override
+                public void handle(MouseEvent event) {
+                    if (event.getClickCount() == 2 && (!row.isEmpty())) {
+                        SingleCoin rowData = row.getItem();
+                        System.out.println(rowData);
+                        String imgPath = rowData.getIconUrl();
 
-                            @Override
-                            public void updateItem(String item, boolean empty) {
-                                super.updateItem(item, empty);
-                                // Change color based on data
-                                if (!isEmpty()) {
-                                    this.setStyle("-fx-text-fill: " + _colorUp + ";-fx-font-weight: bold;");
-                                    if (item.contains("-")) {
-                                        this.setStyle("-fx-text-fill: " + _colorDown + ";-fx-font-weight: bold;");
-                                    }
-                                    setText(item);
-                                }
-                            }
-                        };
+                        // Attempting to resize the coin logo image.
+                        WEB_VIEW.setPrefHeight(56);
+                        WEB_VIEW.setPrefWidth(56);
+                        WEB_VIEW.getEngine().load(imgPath);
                     }
-                });
-            }
+                }
+            });
+            return row;
         });
     }
 
