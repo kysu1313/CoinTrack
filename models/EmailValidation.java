286ca07d1d9ba2686abe391d7f3249b04fe34d14
Haj <69991638+hbadri02@users.noreply.github.com>
2020-11-01 19:46:59 -0500

    EmailValid_2
    Updated API

--- models/EmailValidation.java
+++ models/EmailValidation.java
@@ -10,15 +10,15 @@ import org.json.JSONObject;
  * @author HBadr
  */
 public class EmailValidation {
-    private JSONObject valid;
-
     private final String key = "5a3ce41824msh1c5a64a78ca076dp14b7e4jsn08ee066a23b5";
-    private final String web = "mailcheck.p.rapidapi.com";
-
-     public EmailValidation (String email) {
-            String url = "https://mailcheck.p.rapidapi.com/?domain=" + email;
-            ConnectToApi api = new ConnectToApi(url, this.web, this.key);
-            JSONObject results = api.getJsonObject();
+    private final String web = "email-checker.p.rapidapi.com";
+    private String test;
+     public EmailValidation(String email) {
+        String url = "https://email-checker.p.rapidapi.com/verify/v1?email=" + email;
+        ConnectToApi api = new ConnectToApi(url, this.web, this.key);
+        JSONObject results = api.getJsonObject();
+        //System.out.println(results.toString());
+        test = results.getString("status");
     }
 
 }
